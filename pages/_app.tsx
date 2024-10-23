c6cf45610dc9ddf158cb24fdf4e7a3633d64d952
Nasrullah Nazaruddin <nasrullahnazaruddin@gmail.com>
2024-10-24 00:32:55 +0800

    added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css';
+// pages/_app.tsx
+
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+// Import your global styles if you have any
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
